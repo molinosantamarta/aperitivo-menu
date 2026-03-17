10ff50aa46437b09531fe84d6fca010d811abbfe
Andrea <andrea@mac.local>
2026-03-17 14:45:38 +0100

    Increase format showcase spacing

--- main.runtime.js
+++ main.runtime.js
@@ -24,7 +24,7 @@
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   });
-  const APP_VERSION = "20260317zn";
+  const APP_VERSION = "20260317zo";
   const LOADER_MIN_DURATION = 7e3;
   const FONT_LOAD_TIMEOUT = 2e4;
   const STRICT_FONT_LOAD_TIMEOUT = 45e3;
