32bca87d2775868513f48855f73ccad0c3b39054
Andrea <andrea@mac.local>
2026-03-17 12:54:46 +0100

    Enlarge country format slide

--- main.runtime.js
+++ main.runtime.js
@@ -24,7 +24,7 @@
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   });
-  const APP_VERSION = "20260317r";
+  const APP_VERSION = "20260317s";
   const LOADER_MIN_DURATION = 7e3;
   const FONT_LOAD_TIMEOUT = 2e4;
   const STRICT_FONT_LOAD_TIMEOUT = 45e3;
