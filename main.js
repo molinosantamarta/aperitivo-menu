7e2d703082f31b06cc63b85dba66d30cfc967b36
Andrea <andrea@mac.local>
2026-03-17 12:36:46 +0100

    Tune format carousel slide emphasis

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const priceFormatter = new Intl.NumberFormat("it-IT", {
   maximumFractionDigits: 2,
 });
 
-const APP_VERSION = "20260317l";
+const APP_VERSION = "20260317m";
 const LOADER_MIN_DURATION = 7000;
 const FONT_LOAD_TIMEOUT = 20000;
 const STRICT_FONT_LOAD_TIMEOUT = 45000;
