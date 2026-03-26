92812e05960eda402750d3c4626641caced7d910
Andrea <andrea@mac.local>
2026-03-26 18:32:49 +0100

    Refine mobile beverage visuals and preload

--- src/generated/build-meta.js
+++ src/generated/build-meta.js
@@ -1,4 +1,4 @@
-export const APP_BUILD_NUMBER = 698;
-export const APP_BUILD_SEMVER = "1.0.698";
-export const APP_BUILD_LABEL = "V.1.0.698";
-export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.698";
+export const APP_BUILD_NUMBER = 699;
+export const APP_BUILD_SEMVER = "1.0.699";
+export const APP_BUILD_LABEL = "V.1.0.699";
+export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.699";
