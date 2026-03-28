5450313245f34fa94efa44de8929f87115ac317e
Andrea <andrea@mac.local>
2026-03-28 23:30:29 +0100

    Restore original loader copy

--- src/generated/build-meta.js
+++ src/generated/build-meta.js
@@ -1,4 +1,4 @@
-export const APP_BUILD_NUMBER = 712;
-export const APP_BUILD_SEMVER = "1.0.712";
-export const APP_BUILD_LABEL = "V.1.0.712";
-export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.712";
+export const APP_BUILD_NUMBER = 713;
+export const APP_BUILD_SEMVER = "1.0.713";
+export const APP_BUILD_LABEL = "V.1.0.713";
+export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.713";
