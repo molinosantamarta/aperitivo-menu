7a06413710622b8dcfd042fb511ad308220cdee7
Andrea <andrea@mac.local>
2026-05-05 18:46:42 +0200

    Mark Kombucha unavailable

--- src/generated/build-meta.js
+++ src/generated/build-meta.js
@@ -1,4 +1,4 @@
-export const APP_BUILD_NUMBER = 870;
-export const APP_BUILD_SEMVER = "1.0.870";
-export const APP_BUILD_LABEL = "V.1.0.870";
-export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.870";
+export const APP_BUILD_NUMBER = 871;
+export const APP_BUILD_SEMVER = "1.0.871";
+export const APP_BUILD_LABEL = "V.1.0.871";
+export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.871";
