0521712c951a4364c25ecf56506f01a7df10844e
Andrea <andrea@mac.local>
2026-05-06 12:25:59 +0200

    Update Lurisia orange soda price

--- generated/main.runtime.20260506b.js
+++ generated/main.runtime.20260506b.js
@@ -20,8 +20,8 @@
   var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 
   // src/generated/build-meta.js
-  var APP_BUILD_LABEL = "V.1.0.872";
-  var APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.872";
+  var APP_BUILD_LABEL = "V.1.0.873";
+  var APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.873";
 
   // src/main.js
   window.__agriMenuRuntimeLoaded = true;
@@ -31,7 +31,7 @@
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   });
-  var APP_VERSION = "20260506a";
+  var APP_VERSION = "20260506b";
   var COUNTRY_EVENT_ENABLED_SETTING_KEYS = [
     "country_event_enabled",
     "country_party_enabled",
--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const priceFormatter = new Intl.NumberFormat("it-IT", {
   maximumFractionDigits: 2,
 });
 
-const APP_VERSION = "20260506a";
+const APP_VERSION = "20260506b";
 const COUNTRY_EVENT_ENABLED_SETTING_KEYS = [
   "country_event_enabled",
   "country_party_enabled",
