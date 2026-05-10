8a0b97215f0667048824d017b7f358000810a450
Andrea <andrea@mac.local>
2026-05-11 00:43:17 +0200

    Add Garibaldi drink

--- src/generated/build-meta.js
+++ src/generated/build-meta.js
@@ -1,4 +1,4 @@
-export const APP_BUILD_NUMBER = 885;
-export const APP_BUILD_SEMVER = "1.0.885";
-export const APP_BUILD_LABEL = "V.1.0.885";
-export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.885";
+export const APP_BUILD_NUMBER = 889;
+export const APP_BUILD_SEMVER = "1.0.889";
+export const APP_BUILD_LABEL = "V.1.0.889";
+export const APP_BUILD_FOOTER_LABEL = "VERSIONE 1.0.889";
